a49069af6e6642595e275806412d09727e4f4d6d
joe <jml1074@rit.edu>
2018-09-16 02:23:40 -0400

    basic home page template created

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$('#featuredCarousel').carousel({
+    interval: 10000
+});
+
+$('#kitchenCarousel').carousel({
+    interval: 10000
+});
+
+$('#officeCarousel').carousel({
+    interval: 10000
+});
+
+$('#homeCarousel').carousel({
+    interval: 10000
+});
+
+$('#cleaningCarousel').carousel({
+    interval: 10000
+});
+
+$('#bedroomCarousel').carousel({
+    interval: 10000
+});
+
+$('#bathroomCarousel').carousel({
+    interval: 10000
+});
+
+$('.carousel .carousel-item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i=0;i<2;i++) {
+        next=next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
